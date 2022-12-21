81ccb62cbecb1f9464ae4bd788a0e0161a35fc42
David-F-Chen <davidfaraco.chen@gmail.com>
2022-12-20 22:36:42 -0300

    atualização

--- presentation/alere-play/inicio/alere-play.tsx
+++ presentation/alere-play/inicio/alere-play.tsx
@@ -1,42 +1,131 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './alere-play.module.scss'
-import { Image, Row, Card} from 'antd';
+import { Carousel, Image, Card, Col, Row} from 'antd';
 
-const AlerePlay: React.FC = () => {
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <>
-    <Card className={style.orangeBack}>
-    <Row justify='center' align='middle' gutter={[16, 20]}>
-      <Image
-        preview={{ visible: false }}
-        width={300}
-        src='/assets/alereplay/ALEREPLAY_01.jpg'
-        onClick={() => setVisible(true)}
-      />
-    </Row>
-      <div style={{ display: 'none' }}>
-        <Image.PreviewGroup preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}>
-        <Image width={50} src='/assets/alereplay/ALEREPLAY_02.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_03.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_04.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_05.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_06.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_07.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_08.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_09.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_10.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_11.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_12.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_13.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_14.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_15.jpg'/>
-        <Image width={200} src='/assets/alereplay/ALEREPLAY_16.jpg'/>
-        </Image.PreviewGroup>
-      </div>
-    </Card>
-    </>
-  );
+const contentStyle: React.CSSProperties = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
 };
-export default AlerePlay
+
+const Alereplay: React.FC = () => (
+  <Card className={style.orangeBack}>
+    <Carousel autoplay autoplaySpeed={2000}>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_01.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_02.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_03.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_04.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_05.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_06.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_07.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_08.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_09.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_10.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_11.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_12.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_13.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_14.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_15.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+    <div>
+    <Col xl={24} md={24} sm={24} className={style.yellowBack}>
+      <Row justify='center' align='middle' gutter={[16, 20]}>
+        <Image width={1000} src='/assets/alereplay/ALEREPLAY_16.png' preview={false} />
+      </Row>
+    </Col>
+    </div>
+  </Carousel>
+</Card>
+)
+export default Alereplay;
--- presentation/catalogo-alere/catalogo-alere.tsx
+++ presentation/catalogo-alere/catalogo-alere.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import style from './catalogo-alere.module.scss'
-import { Image, Card, Carousel} from 'antd'
+import { Image, Row, Col, Card} from 'antd';
 
-const CatalogoAlere: React.FC = () => (
-  <Card className={style.orangeBack}>
-    <Carousel autoplay autoplaySpeed={3000}>
-        <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-01.png'/>
+const CatalogoAlere: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <>
+    <Card className={style.orangeBack}>
+    <Row justify='center' align='middle' gutter={[16, 20]}>
+      <Image
+        preview={{ visible: false }}
+        width={1200}
+        src='/assets/catalogoalere/Topo.png'
+        onClick={() => setVisible(true)}
+      />
+    </Row>
+      <div style={{ display: 'none' }}>
+        <Image.PreviewGroup preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}>
+        return(<Row justify='center' align='middle' gutter={[16, 20]}>
+          <Image src='/assets/catalogoalere/Catálogo Livros Digitais Alere-01.png'preview={true} />
+          <h1>A coleção Revisão dos Saberes tem como objetivo</h1>
+        </Row>
+        )
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-02.png'/>
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-03.png'/>
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-04.png'/>
@@ -18,8 +34,11 @@ const CatalogoAlere: React.FC = () => (
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-11.png'/>
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-12.png'/>
         <Image width={500} src='/assets/catalogoalere/Catálogo Livros Digitais Alere-13.png'/>     
-    </Carousel>
-   </Card>
-);
+        </Image.PreviewGroup>
+      </div>
+    </Card>
+    </>
+  );
+};
 
 export default CatalogoAlere
